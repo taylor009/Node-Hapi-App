da68820564620c6bf823444877d7a43371eec4f9
taylor009 <taylor@baremetalcloud.com>
2019-01-03 22:29:52 -0500

    Added Bell auth-cookie package from npm and configured it within app

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -1,21 +1,24 @@
 "use strict";
-const Blipp = require("blipp");
-const HapiPino = require("hapi-pino");
-const serverStart = require("./serverStart");
+
+const blipp = require( "blipp" );
+const pino = require( "hapi-pino" );
+const serverStart = require( "./serverStart" );
+const auth = require( "./auth" );
 
 const isDev = process.env.NODE_ENV !== "production";
 
 module.exports.register = async server => {
-   await server.register([Blipp, {
-       plugin: HapiPino,
-       options: {
-           prettyPrint: isDev,
-           logEvents: ["response"]
-       }
-   },{
-       plugin: serverStart,
-       options: {
-           message: `My hapi server is running at ${ server.info.uri }`
-       }
-   }]);
+    await server.register( [ blipp, {
+        plugin: pino,
+        options: {
+            prettyPrint: isDev,
+            logEvents: [ "response" ]
+        }
+    }, {
+        plugin: serverStart,
+        options: {
+            message: `My hapi server is running at ${ server.info.uri }`
+        }
+    } ] );
+    await auth.register( server );
 };
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,20 +1,76 @@
 "use strict";
 
+const boom = require( "boom" );
+
 module.exports.register = async server => {
     server.route( {
         method: "GET",
         path: "/",
-        // a handler can return text, HTML, JSON,
-        // a file, or just about anything
-        handler: async (request, h) => {
-            try{
-                const message = "My first hapi server!";
-                return h.view("index", {
+        config: {
+            auth: {
+                strategy: "session",
+                mode: "optional"
+            }
+        },
+        handler: async ( request, h ) => {
+            try {
+                const message = request.auth.isAuthenticated ? `Hello, ${ request.auth.credentials.profile.firstName }!` : "My first hapi server!";
+                return h.view( "index", {
                     title: "Home",
-                    message
-                });
-            } catch (err) {
-                server.log(["error", "home"], err)
+                    message,
+                    isAuthenticated: request.auth.isAuthenticated
+                } );
+            } catch ( err ) {
+                server.log( [ "error", "home" ], err );
+            }
+        }
+    } );
+
+    server.route( {
+        method: "GET",
+        path: "/login",
+        options: {
+            auth: "session",
+            handler: async request => {
+                return `Hello, ${ request.auth.credentials.profile.email }!`;
+            }
+        }
+    } );
+
+    server.route( {
+        method: "GET",
+        path: "/authorization-code/callback",
+        options: {
+            auth: "okta",
+            handler: ( request, h ) => {
+                if ( !request.auth.isAuthenticated ) {
+                    throw boom.unauthorized( `Authentication failed: ${ request.auth.error.message }` );
+                }
+                request.cookieAuth.set( request.auth.credentials );
+                return h.redirect( "/" );
+            }
+        }
+    } );
+
+    server.route( {
+        method: "GET",
+        path: "/logout",
+        options: {
+            auth: {
+                strategy: "session",
+                mode: "try"
+            },
+            handler: ( request, h ) => {
+                try {
+                    if ( request.auth.isAuthenticated ) {
+                        // clear the local session
+                        request.cookieAuth.clear();
+                    }
+
+                    return h.redirect( "/" );
+                } catch ( err ) {
+                    request.log( [ "error", "logout" ], err );
+                }
             }
         }
     } );
