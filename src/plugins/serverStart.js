8168623647e4e48164b54c97d9a8b42a7bed19b7
taylor009 <taylor@baremetalcloud.com>
2019-01-03 22:05:18 -0500

    Added EJS view dependencies and created a layout view

--- src/plugins/serverStart.js
+++ src/plugins/serverStart.js
@@ -3,10 +3,10 @@
 module.exports = {
     name: "serverStart",
     version: "1.0.0",
-    register: async (server, {message}) => {
-        server.event.on('start', () => {
+    register: async ( server, { message } ) => {
+        server.events.on( "start", () => {
             const msg = message || `Server running at ${ server.info.uri }`;
-            server.log(["info", "server"], msg)
-        });
+            server.log( [ "info", "server" ], msg );
+        } );
     }
 };
